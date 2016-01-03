465c1ecb7e03b30f15e82874dd1d13f97f854f2f
zsyjx0115 <zsyjx0115@163.com>
2016-01-03 22:00:55 +0800

    add search function

--- assets/themes/myfirst-theme/js/top.js
+++ assets/themes/myfirst-theme/js/top.js
@@ -33,3 +33,19 @@ $(document).ready(function(){
 		},50);
 	});
 });	
+
+
+//search js function
+(function(w,d,t,u,n,s,e){
+	w['SwiftypeObject']=n;
+	w[n]=w[n]||function(){
+	  (w[n].q=w[n].q||[]).push(arguments);
+	};
+	s=d.createElement(t);
+	e=d.getElementsByTagName(t)[0];
+	s.async=1;
+	s.src=u;
+	e.parentNode.insertBefore(s,e);
+	})(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+
+_st('install','po5qdFZDk2T1AX6ThzTT','2.0.0');
